e88fc58c7ce121db4bbcb6f785c4b9bfb322fe12
ruhul63rrss <ruhulamin63rrss@gmail.com>
2023-01-31 00:29:55 +0600

    First commit

--- nthLargestNumber.py
+++ nthLargestNumber.py
@@ -9,17 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    
-    a = []
-
-    for e in range(1,12,1):
-        if (e%2) == 0:
-            a.append(e)
-    return a
+    # List of Integers
+    numbers = [12, 33, 44, 12,37,48,57,58]
+ 
+    #Sorting list of Integers
+    numbers.sort()
+    return "2nd largest number from the given list is "+ str(numbers[1])
 
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
     # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host='192.168.0.114', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
