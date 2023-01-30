90240b1169f9b1c8295b3ff370198aad94166317
ruhul63rrss <ruhulamin63rrss@gmail.com>
2023-01-31 00:42:46 +0600

    2nd commit

--- evenNumber.py
+++ evenNumber.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
     # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host='192.168.0.114', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
